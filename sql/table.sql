65c57d38f56e62b6fce893d074b2104e11541cf1
YOKOMIZO, Kazumasa <Kazumasa@yokomizohajimeshou-no-MacBook-Pro.local>
2015-07-03 00:42:07 +0900

    reset

--- sql/table.sql
+++
@@ -1,4 +0,0 @@
-create table users (
-    name varchar(255) primary key,
-    email text not null
-);
--- sql/test_data.sql
+++
@@ -1,3 +0,0 @@
-insert into users values ('user1', 'user1@example.com');
-insert into users values ('user2', 'user2@example.com');
-insert into users values ('user3', 'user3@example.com');
