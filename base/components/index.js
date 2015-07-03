58e1ca0c39494ac14e49ad9bbf6d03f051b37690
YOKOMIZO, Kazumasa <Kazumasa@yokomizohajimeshou-no-MacBook-Pro.local>
2015-07-03 22:07:45 +0900

    fource

---
+++ base/components/index.js
@@ -0,0 +1,114 @@
+var todos = [{
+	id 		: 	'_id',
+	name	: 	'Buy some milk',
+	done 	: 	true
+}, {
+	id 		: 	'_id',
+	name 	: 	'Birthday present to alice',
+	done	: 	false
+}];
+
+var Todo = React.createClass({
+	render: function(){
+		var todo = this.props.todo;
+		return (<li>{todo.name}<button>Done</button></li>);
+	}
+});
+
+var TodoList = React.createClass({
+	render: function(){
+		var rows = this.props.todos.filter(function(todo){
+			return !todo.done;
+		}).map(function(todo){
+			return (<Todo key={todo.id} todo={todo}></Todo>);
+		});
+		return (
+			<div className="active-todos">
+				<h2>Active</h2>
+				<ul>{rows}</ul>
+			</div>
+		);
+	}
+});
+
+var TodoForm = React.createClass({
+	getInitialState: function(){
+		return {
+			name: ''
+		};
+	},
+	handleNameChange: function(e){
+		this.setState({
+			name: e.target.value
+		});
+	},
+	handleSubmit: function(e){
+		e.preventDefault();
+		var name = this.state.name.trim();
+		//1、データを受け取る
+		TodoStorage.create(name, function(){
+			this.setState({
+				name: ''
+			});
+		}.bind(this));
+	},
+	render: function(){
+		var disabled = this.state.name.trim().length <= 0;
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input value={this.state.name} onChange={this.handleNameChange}></input>
+				<input type="submit" disabled{disabled}></input>
+			</form>
+		)
+	}
+
+	handleSubmit: function(e){
+		e.preventDefault();
+		var name = this.refs.todoName.getDOMNode().value.trim();
+		if(name){
+			alert(name);
+			this.refs.todoName.getDOMNode().value = '';
+		}
+	},
+	render: function(){
+		return (
+			<form onSubmit={this.handleSubmit}>
+				<input ref="todoName"></input><input type="submit"></input>
+			</form>
+		);
+	}
+});
+
+
+var App = React.createClass({
+	getInitialState: function(){
+		return {
+			todos: []
+		};
+	},
+	componentDidMount: function(){
+		var setTodos = function(){
+			TodoStorage.getAll(function(todos){
+				this.setState({
+					todos: todos
+				});
+			}.bind(this));
+		}.bind(this);
+		//２フィードバックを受け取る
+		TodoStorage.on('change', setTodos);
+		setTodos();
+	},
+	render: function(){
+		return (
+			<div>
+				<h1>My Todo</h1>
+				<TodoList todos={todos}/>
+			</div>
+		);
+	}
+});
+
+React.render(
+	<App></App>
+	document.getElementById('app-container')
+);
